9edf658af081d2c2a76e38e08a4fc401ed3f3fe8
cammylexi <167337765+cammylexi@users.noreply.github.com>
2024-04-19 11:48:59 -0500

    Update ItemToPurchase.cpp

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -4,3 +4,19 @@ using namespace std;
 #include "ItemToPurchase.h"
 
 /* Type your code here */
+
+ItemToPurchase::SetName(){
+name = 
+} 
+
+ItemToPurchase::GetName(){
+name = 
+} 
+  ItemToPurchase::SetQuantity (){
+name = 
+} 
+  ItemToPurchase::GetQuanity(){
+quantity = 
+} 
+
+
